1cfd5958f6d80cc5668237293165fb362ad3e5e0
Bmznn <questionmark19@outlook.com>
2024-09-12 17:44:48 +0100

    initial commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ authGui.title("Username Sniper")
 authGui.geometry('400x300')
 authGui.resizable(False, False)
 
-def attempt_Login():
+def attemptLogin():
     username = authUser.get()
     password = authPass.get()
 
@@ -18,14 +18,14 @@ def attempt_Login():
         print("Authenticated as:", user_profile.name)
         messagebox.showinfo("Login Status", "Login Successful")
 
-        open_new_window(client, user_profile)
+        openNewWindow(client, user_profile)
 
 
 
     except Exception as e:
         messagebox.showerror("Login Status", "Login Failed")
 
-def open_new_window(client, user_profile):
+def openNewWindow(client, user_profile):
     client.get_profile()
     authGui.destroy()
     usernameGui = Tk()
@@ -33,64 +33,64 @@ def open_new_window(client, user_profile):
     usernameGui.geometry('417x231')
     usernameGui.resizable(False, False)
 
-    console_frame = Frame(usernameGui)
-    console_frame.pack(side=TOP, fill=BOTH, expand=True, padx=10, pady=11)
-    console_frame.grid(column=0, row=1, columnspan=2)
+    consoleFrame = Frame(usernameGui)
+    consoleFrame.pack(side=TOP, fill=BOTH, expand=True, padx=10, pady=11)
+    consoleFrame.grid(column=0, row=1, columnspan=2)
 
-    console_text = Text(console_frame, height=8, width=40, wrap='word', state='disabled',)
-    console_text.pack(side=LEFT, fill=BOTH)
+    consoleText = Text(consoleFrame, height=8, width=40, wrap='word', state='disabled',)
+    consoleText.pack(side=LEFT, fill=BOTH)
 
-    scrollbar = Scrollbar(console_frame, command=console_text.yview)
+    scrollbar = Scrollbar(consoleFrame, command=consoleText.yview)
     scrollbar.pack(side=RIGHT, fill=Y)
 
-    console_text.config(yscrollcommand=scrollbar.set)
+    consoleText.config(yscrollcommand=scrollbar.set)
 
-    def log_to_console(message):
-        console_text.config(state='normal')
-        console_text.insert(END, message + '\n')
-        console_text.config(state='disabled')
-        console_text.yview(END)
+    def logToConsole(message):
+        consoleText.config(state='normal')
+        consoleText.insert(END, message + '\n')
+        consoleText.config(state='disabled')
+        consoleText.yview(END)
 
     def performCheck():
         targetUser = usernameBox.get()
         result = client.is_username_available(targetUser)
 
         if result:
-            log_to_console(f"{targetUser} is available")
+            logToConsole(f"{targetUser} is available")
             return True
         else:
-            log_to_console(f"{targetUser} is not available")
+            logToConsole(f"{targetUser} is not available")
             return False
 
     def snipeyTime():
         targetUser = usernameBox.get()
         if performCheck():
-            log_to_console(f"Attempting to change username to {targetUser}...")
+            logToConsole(f"Attempting to change username to {targetUser}...")
             try:
                 response = client.change_username(targetUser)
                 if response.get('success'):
-                    log_to_console(f"{targetUser} has been sniped")
+                    logToConsole(f"{targetUser} has been sniped")
                 else:
-                    log_to_console(f"{targetUser} failed to snipe: {response.get('error', 'Unknown Error')}")
+                    logToConsole(f"{targetUser} failed to snipe: {response.get('error', 'Unknown Error')}")
             except Exception as e:
-                log_to_console(f"ERROR {e}")
+                logToConsole(f"ERROR {e}")
         else:
-            log_to_console(f"Attempting to snipe username again...")
+            logToConsole(f"Attempting to snipe username again...")
             usernameBox.after(1000, snipeyTime)
 
-    top_frame = Frame(usernameGui)
-    top_frame.grid(row=0, column=0, padx=10, pady=10)
+    topFrame = Frame(usernameGui)
+    topFrame.grid(row=0, column=0, padx=10, pady=10)
 
-    usernamePrompt = Label(top_frame, text="Username:",font=("Arial", 16, "bold"))
+    usernamePrompt = Label(topFrame, text="Username:",font=("Arial", 16, "bold"))
     usernamePrompt.grid(column=0, row=0)
 
-    usernameBox = Entry(top_frame, font=("Arial", 16, "bold"), width=15)
+    usernameBox = Entry(topFrame, font=("Arial", 16, "bold"), width=15)
     usernameBox.grid(column=1, row=0)
 
-    usernameCheck = Button(top_frame, text="Check Availability", anchor='center',font=("Arial", 16, "bold"), command=performCheck)
+    usernameCheck = Button(topFrame, text="Check Availability", anchor='center',font=("Arial", 16, "bold"), command=performCheck)
     usernameCheck.grid(column=0, row=1)
 
-    usernameSnipe = Button(top_frame, text="Snipe", anchor='center', font=("Arial", 16, "bold"), width=15, command=snipeyTime)
+    usernameSnipe = Button(topFrame, text="Snipe", anchor='center', font=("Arial", 16, "bold"), width=15, command=snipeyTime)
     usernameSnipe.grid(column=1, row=1)
 
 
@@ -110,7 +110,7 @@ authPass.grid(column =0, row=3)
 lineBreak = Label(authGui, text="---------------------------------------", font=("Arial", 16, "bold"))
 lineBreak.grid(column =0, row=4)
 
-auth = Button(authGui, text="Authenticate", font = ("Arial", 16, "bold"), width=30, height=1, activebackground="green3", disabledforeground="gray15", bg="gray62", command=attempt_Login)
+auth = Button(authGui, text="Authenticate", font = ("Arial", 16, "bold"), width=30, height=1, activebackground="green3", disabledforeground="gray15", bg="gray62", command=attemptLogin)
 auth.grid(column =0, row=5)
 
 authGui.mainloop()
